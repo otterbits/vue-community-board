e8d84c88a2fc3a24fb63ae5a5722aa3283a4fe02
uoohoon <hoonixox@naver.com>
2023-12-11 20:41:11 +0900

    feat: create post detail

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/views/PageHome.vue'
 import BoardList from '@/views/board/BoardList.vue'
 import BoardDetail from '@/views/board/BoardDetail.vue'
+import BoardWrite from '@/views/board/BoardWrite.vue'
 
 const routes = [
     {
@@ -26,7 +27,12 @@ const routes = [
         path: '/board/detail',
         name: 'BoardDetail',
         component: BoardDetail
-      },
+    },
+    {
+        path: '/board/write',
+        name: 'BoardWrite',
+        component : BoardWrite
+    },
 ]
 
 const router = createRouter({
