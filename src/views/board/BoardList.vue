cb73dd3c1e9912b03db6d22908c941b93b9d9a5b
uoohoon <hoonixox@naver.com>
2023-12-11 21:13:35 +0900

    feat: add post searching box
    feat: add board page list
    
    faet: add post searching box
    
    faet: add post searching box

--- src/views/board/BoardList.vue
+++ src/views/board/BoardList.vue
@@ -40,6 +40,19 @@
         </span>
     </div>
 </div>
+
+<div>
+    <select v-model="search_key">
+      <option value="">- 선택 -</option>
+      <option value="author">작성자</option>
+      <option value="title">제목</option>
+      <option value="contents">내용</option>
+    </select>
+    &nbsp;
+    <input type="text" v-model="search_value" @keyup.enter="fnPage()">
+    &nbsp;
+    <button @click="fnPage()">검색</button>
+</div>
 </template>
 
 <script>
@@ -64,7 +77,8 @@ export default {
         }, //페이징 데이터
         page: this.$route.query.page ? this.$route.query.page : 1,
         size: this.$route.query.size ? this.$route.query.size : 10,
-        keyword: this.$route.query.keyword,
+        search_key: this.$route.query.sk ? this.$route.query.sk : '',
+        serach_value: this.$route.query.sv ? this.$route.query.sv : '',
         paginavigation: function () { //페이징 처리 for문 커스텀
         let pageNumber = [] //;
         let start_page = this.paging.start_page;
@@ -81,7 +95,8 @@ mounted() {
 // methods: {
 //     fnGetList() {
 //       this.requestBody = { // 데이터 전송
-//         keyword: this.keyword,
+//         sk: this.search_key,
+//         sv: this.search_value,
 //         page: this.page,
 //         size: this.size
 //       }
@@ -89,9 +104,14 @@ mounted() {
 //       this.$axios.get(this.$serverUrl + "/board/list", {
 //         params: this.requestBody,
 //         headers: {}
-//       }).then((res) => {      
+//       }).then((res) => {
 
-//         this.list = res.data  //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
+//           if (res.data.result_code === "OK") { //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
+//             this.list = res.data.data
+//             this.paging = res.data.pagination
+//             this.no = this.paging.total_list_cnt - ((this.paging.page - 1) * this.paging.page_size)
+//           }
+// //         this.list = res.data  
 
 //       }).catch((err) => {
 //         if (err.message.indexOf('Network Error') > -1) {
@@ -138,8 +158,9 @@ methods: {
     fnPage(n) {
       if (this.page !== n) {
         this.page = n
-        this.fnGetList()
       }
+
+      this.fnGetList()
     }
 }
 }
