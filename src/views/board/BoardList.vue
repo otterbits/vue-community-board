7d7516a6b395a8611ba89a0eadb99c1666134972
uoohoon <hoonixox@naver.com>
2023-12-11 18:21:06 +0900

    test: get post from server

--- src/views/board/BoardList.vue
+++ src/views/board/BoardList.vue
@@ -77,6 +77,30 @@ export default {
 mounted() {
     this.fnGetList()
 },
+// 서버에서 게시글 가져와서 출력하기
+// methods: {
+//     fnGetList() {
+//       this.requestBody = { // 데이터 전송
+//         keyword: this.keyword,
+//         page: this.page,
+//         size: this.size
+//       }
+
+//       this.$axios.get(this.$serverUrl + "/board/list", {
+//         params: this.requestBody,
+//         headers: {}
+//       }).then((res) => {      
+
+//         this.list = res.data  //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
+
+//       }).catch((err) => {
+//         if (err.message.indexOf('Network Error') > -1) {
+//           alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
+//         }
+//       })
+//     }
+//   }
+// }
 methods: {
     fnGetList() {
         this.list = [
