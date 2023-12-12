22a38b9bc3c53bb5fb75438c9cef167d5501d154
uoohoon <hoonixox@naver.com>
2023-12-12 20:04:47 +0900

    refactor: switch Home, login tap (top list)

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -3,9 +3,9 @@
     <header>
         <div id="nav">
             <router-link to="/">Home</router-link> |
+            <router-link to="/login">로그인</router-link> |
             <router-link to="/about">About</router-link> |
-            <router-link to="board/list">게시판</router-link> |
-            <router-link to="/login">로그인</router-link>
+            <router-link to="board/list">게시판</router-link>
         </div>
     </header>
     <hr/>
