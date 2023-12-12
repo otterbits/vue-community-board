f3d8b4b9b86f181da30fcbb8aee96e6d9cd5a695
uoohoon <hoonixox@naver.com>
2023-12-12 14:19:58 +0900

    feat: set Vuex package

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import store from './vuex/store'
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;  //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있음
 app.config.globalProperties.$serverUrl = '//localhost:8081' //api server
-app.use(router).mount('#app')
+app.config.globalProperties.$store = store
+app
+    .use(router)
+    .use(store)
+    .mount('#app')
