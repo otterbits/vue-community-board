f1a1ebea98cadb936a5675b8f87580d826460706
uoohoon <hoonixox@naver.com>
2023-12-12 15:34:50 +0900

    implement login API

--- src/views/common/Login.vue
+++ src/views/common/Login.vue
@@ -21,6 +21,7 @@
 </template>
 
 <script>
+import {mapActions, mapGetters} from 'vuex'
 
 export default {
     data() {
@@ -30,7 +31,9 @@ export default {
         }
     },
     methods: {
-        fnLogin() {
+        ...mapActions(['login']),
+
+        async fnLogin() {
             if (this.user_id === '') {
                 alert('ID를 입력하세요.')
                 return
@@ -39,8 +42,24 @@ export default {
                 alert('비밀번호를 입력하세요')
                 return
             }
-            alert('로그인 되었습니다.')
+
+            //로그인 API 호출
+            try {
+                let loginResult = await this.login({user_id: this.user_id, user_pw: this.user_pw})
+                if (loginResult) alert('로그인 결과 : ' + loginResult)
+            } catch (err) {
+                if (err.message.indexOf('Network Error') > -1) {
+                    alert('서버에 접속할 수 없습니다. 상태를 확인해주세요.')
+                } else {
+                    alert('로그인 정보를 확인할 수 없습니다.')
+                }
+            }
         }
+    },
+    computed: {
+        ...mapGetters({
+            errorState: 'getErrorState'
+        })
     }
 }
 </script>
