5aef85f50f70ca70ad3efa3a1585bf570ed9900a
uoohoon <hoonixox@naver.com>
2023-12-17 16:23:34 +0900

    remove: remove Helloworld.vue in PageHome

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -2,17 +2,14 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" width="300" height="300">
-        <HelloWorld msg="Welcome to otterbit Vue.js Webpage"/>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
     name: 'PageHome',
-    components: {
-        HelloWorld
-    }
-}</script>
+   
+}
+</script>
