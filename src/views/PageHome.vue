8355712dae28c7315a2b51cc985452722d5e7436
uoohoon <hoonixox@naver.com>
2023-12-20 21:14:44 +0900

    refactor: change Aritcle.vue name to BoardCard.vue

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,18 +1,18 @@
 <!-- PageHome.vue -->
 <template>
     <div class="home">
-    <Article/>
+    <BoardCard/>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Article from './Article/Article.vue'
+import BoardCard from './board/BoardCard.vue';
 
 export default {
     name: 'PageHome',
     components: {
-        Article : Article
-    }
+    BoardCard: BoardCard
+}
 }
 </script>
